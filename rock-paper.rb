7adf9d747b5b28f7f0738f38cdc08f170c2e55f3
Dane Carmichael <carmichaeldane@gmail.com>
2016-03-08 20:09:45 -0600

    Finished explorer mode, just needs cleaned up.

--- rock-paper.rb
+++ rock-paper.rb
@@ -18,7 +18,40 @@ class RoShamBo
     @computer_choice = ["ROCK", "PAPER", "SCISSORS"][(rand(0..2))]
   end
   def play_ro_sham
-    puts "Player chose #{@choice}. Computer chose #{@computer_choice}."
+    if @choice == "ROCK"
+      rock_logic(@computer_choice)
+    elsif @choice == "PAPER"
+      paper_logic(@computer_choice)
+    else
+      scissors_logic(@computer_choice)
+    end
+  end
+  def rock_logic(c_choice)
+    if c_choice == "ROCK"
+      puts "Player: Rock vs Computer: Rock ---- Tie."
+    elsif c_choice == "PAPER"
+      puts "Player: Rock vs Computer: Paper ---- Computer wins..."
+    else
+      puts "Player: Rock vs Computer: Scissors ---- Player wins!"
+    end
+  end
+  def paper_logic(c_choice)
+    if c_choice == "ROCK"
+      puts "Player: Paper vs Computer: Rock ---- Player wins!"
+    elsif c_choice == "PAPER"
+      puts "Player: Paper vs Computer: Paper ---- Tie."
+    else
+      puts "Player: Paper vs Computer: Scissors ---- Computer wins..."
+    end
+  end
+  def scissors_logic(c_choice)
+    if c_choice == "ROCK"
+      puts "Player: Scissors vs Computer: Rock ---- Computer wins..."
+    elsif c_choice == "PAPER"
+      puts "Player: Scissors vs Computer: Paper ---- Player wins!"
+    else
+      puts "Player: Scissors vs Computer: Scissors ---- Tie."
+    end
   end
 end
 
@@ -26,10 +59,12 @@ puts "Lets play Paper-Rock-Scissors."
 
 choice = gets.chomp.upcase
 
-# unless choice == "ROCK" || "PAPER" || "SCISSORS"
-#   puts "Please make a valid choice of rock, paper, or scissors."
-#   choice = gets.chomp.upcase
-# end
+unless choice == "ROCK" ||
+      choice == "PAPER" ||
+      choice == "SCISSORS"
+  puts "Please make a valid choice of rock, paper, or scissors."
+  choice = gets.chomp.upcase
+end
 
 new_game = RoShamBo.new(choice)
 new_game.play_ro_sham
